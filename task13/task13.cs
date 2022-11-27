16af58b15cb5608d61560d75d24c970e70ed685f
Danis <danshadance@mail.ru>
2022-11-27 09:00:22 +0300

    Пробую решать 13 задачу

--- task13/task13.cs
+++ task13/task13.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вводим любое число.
+// Если число трехзначное - выводим третью цифру этого числа.
+// Если числе НЕ трехзначное - выводим сообщение что третьей цифры нет.
+
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num3 < 100 | num3 > -100)
+{
+    Console.WriteLine("Ошибка ввода.Число не является трехзначным.");
+}
+else
+{
+    if(num3 > 99 | num3 < -99)
+    {
+        int last = num3 % 10;
+        Console.WriteLine(last);
+    }
+}
