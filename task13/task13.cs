b626a2a0f85271d1503a609e1ed8c821085df46d
Danis <danshadance@mail.ru>
2022-11-27 17:58:42 +0300

    большой алгоритм для 9 знаков

--- task13/task13.cs
+++ task13/task13.cs
@@ -8,19 +8,48 @@ if (num > -100 && num < 100)
 {
     Console.WriteLine("Ошибка ввода.Введите минимум трехзначное число.");
 }
-else
-{ 
-  if(num > -1000 && num < -99 | num > 99 && num < 1000)
-  {
-    num = num % 10;
-    Console.WriteLine(num);
-  }
-}
 
-if(num < -1000 | num > 1000)
+if(num > -1000 && num < -99 | num > 99 && num < 1000) // 3 знака
 {
-    num = num / 10;
     num = num % 10;
     Console.WriteLine(num);
 }
 
+if(num > -10000 && num < -999 | num > 999 && num < 10000) // 4 знака
+{
+    num = (num / 10) % 10;
+    Console.WriteLine(num);
+}
+
+if(num > -100000 && num < -9999 | num > 9999 && num < 100000) // 5 знаков
+{
+    num = (num / 100) % 10;
+    Console.WriteLine(num);
+}
+
+if(num > -1000000 && num < -99999 | num > 99999 && num < 1000000) // 6 знаков
+{
+    num = (num / 1000) % 10;
+    Console.WriteLine(num);
+}
+
+if(num > -10000000 && num < -999999 | num > 999999 && num < 10000000) // 7 знаков
+{
+    num = (num / 10000) % 10;
+    Console.WriteLine(num);
+}
+
+if(num > -100000000 && num < -9999999 | num > 9999999 && num < 100000000) // 8 знаков
+{
+    num = (num / 100000) % 10;
+    Console.WriteLine(num);
+}
+
+if(num > -1000000000 && num < -99999999 | num > 99999999 && num < 1000000000) // 9 знаков
+{
+    num = (num / 1000000) % 10;
+    Console.WriteLine(num);
+}
+
+// Как это решить через метод пока не понял
+// Так же если вводить число с минусом, то он будет показывать минус в результате
