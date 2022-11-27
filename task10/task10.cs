e1565def21ed0c7c079374935f067d56a37961c4
Danis <danshadance@mail.ru>
2022-11-27 08:25:24 +0300

    task10 v1.0

--- task10/task10.cs
+++ task10/task10.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На вход принимаем трехзначное число 
+// На выходе показываем вторую цифру этого числа 
+
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int res = (num3 / 10) % 10;
+
+Console.WriteLine(res);
